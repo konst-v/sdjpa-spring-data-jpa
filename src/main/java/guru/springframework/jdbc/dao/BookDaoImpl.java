2cf5f7a2a1987e6030b45e114c0eb41a977711dc
konst-v <konst.vv@gmail.com>
2022-03-21 21:52:29 +0200

    Spring data JPA

---
+++ src/main/java/guru/springframework/jdbc/dao/BookDaoImpl.java
@@ -0,0 +1,49 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Book;
+import guru.springframework.jdbc.repositories.BookRepository;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+
+@Component
+public class BookDaoImpl implements BookDao{
+    private BookRepository bookRepository;
+
+    public BookDaoImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+
+    @Override
+    public Book getById(Long id) {
+        return bookRepository.getById(id);
+    }
+
+    @Override
+    public Book findBookByTitle(String title) {
+        return bookRepository.findBookByTitle(title)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Override
+    public Book saveNewBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Transactional
+    @Override
+    public Book updateBook(Book book) {
+        Book foundBook = bookRepository.getById(book.getId());
+        foundBook.setTitle(book.getTitle());
+        foundBook.setPublisher(book.getPublisher());
+        foundBook.setIsbn(book.getIsbn());
+        return bookRepository.save(foundBook);
+    }
+
+    @Override
+    public void deleteBookById(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
--- src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
+++ src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
@@ -2,6 +2,35 @@ package guru.springframework.jdbc.repositories;
 
 import guru.springframework.jdbc.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+import java.util.concurrent.Future;
+import java.util.stream.Stream;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Book jpaNamed(@Param("title") String title);
+
+    @Query(value = "select * from book where title = :title", nativeQuery = true)
+    Book findBookByTitleNativeQuery(@Param("title") String title);
+
+    @Query("select b from Book b where b.title = :title")
+    Book findBookByTitleWithQueryNamed(@Param("title") String title);
+
+    @Query("select b from Book b where b.title = ?1")
+    Book findBookByTitleWithQuery(String title);
+
+    Optional<Book> findBookByTitle(String title);
+
+    Book readByTitle(String title);
+
+    @Nullable
+    Book getByTitle(@Nullable String title);
+
+    Stream<Book> findAllByTitleNotNull();
+
+    Future<Book> queryByTitle(String title);
 }
